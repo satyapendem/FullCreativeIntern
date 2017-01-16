946f04687f39184aa6e3026160f524b6592c0603
satyapendem <sunilsmartguy005@gmail.com>
2017-01-16 18:41:08 +0530

    GuestBook was added

---
+++ week2/GuestBook/src/com/guestbook/GuestbookServlet.java
@@ -0,0 +1,17 @@
+package com.guestbook;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestbookServlet extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    GuestbookUtils.insert(req.getParameter("who"), req.getParameter("message"));
+    resp.sendRedirect("/guestbook.jsp");
+  }
+}
