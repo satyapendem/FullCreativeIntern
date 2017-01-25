377c86daaa63ffa92de1a11de19edc1579985cd5
satyapendem <sunilsmartguy005@gmail.com>
2017-01-25 11:40:40 +0530

    In core java badge programs are edited

--- week2/Core Java Badge/Constructor_Ex.java
+++ week2/Core Java Badge/Constructor_Ex.java
@@ -1,17 +1,29 @@
-class Constructor_Ex{  
+class A
+{
+  int a;
+  String b; 
+
+  A(int x){
+  	this.a=x;
+  	System.out.println("The id n base class is:"+a);
+  } 
+}
+class Constructor_Ex extends A{  
     int id;  
     String name;  
       
-    Constructor_Ex(int i,String n){  
-    id = i;  
-    name = n;  
+    Constructor_Ex(int id,String name){  
+    super(id);
+    this.id = id;  
+    this.name = name;  
     }  
     void display(){
+    	
         System.out.println(id+" "+name);
     }  
    
     public static void main(String args[]){  
-    Constructor_Ex s1 = new Constructor_Ex(111,"satya");  
+    Constructor_Ex s1 = new Constructor_Ex(515,"satya");  
     Constructor_Ex s2 = new Constructor_Ex(516,"chaitu");  
     s1.display();  
     s2.display();  
--- week2/Core Java Badge/ListMapDemo.java
+++ week2/Core Java Badge/ListMapDemo.java
@@ -2,21 +2,53 @@ import java.util.*;
 public class ListMapDemo{
 
    public static void main(String[] args) {
+
+      /*Information about list
+        1.List class can contain duplicate elements.
+        2.List class maintains insertion order.
+        3.when we call list the default size is 10.
+        4.We will use list when there is a need of duplicates. 
+      */
       List a1 = new ArrayList();
       a1.add("A");
       a1.add("B");
       a1.add("C");      
       System.out.println(" ArrayList Elements");
       System.out.print("\t" + a1);
+      /*
 
+        1.It will store data in the form of Key,value pair.
+        2.It does not maintain insertion order.
+        3.It stores data in a sorted oder.
+        4.It does not support but it supports null values.
+      */
      Map m1 = new HashMap(); 
-      m1.put("satya", "515");
-      m1.put("chaitu", "516");
-      m1.put("reddy", "521");
-      m1.put("siva", "527");
-
+      m1.put(515,"satya");
+      m1.put(516,"chaitu");
+      m1.put(527,"siva");
+      m1.put(521, "redddy");
       System.out.println();
       System.out.println(" Map Elements");
       System.out.print("\t" + m1);
+
+      /*
+        1.A set does not allow duplicate values.
+        2.A set can allow one null value.
+        3.It can store values based on hashing.
+        4.It does not maintain insertion order.
+      */
+   HashSet<String> set=new HashSet<String>();  
+  set.add(null);
+  set.add("satya");  
+  set.add("chaitu");  
+  set.add("redddy");  
+  set.add("satya");    
+  System.out.println();
+  System.out.println("The elements in set are:"); 
+  Iterator<String> itr=set.iterator();  
+  while(itr.hasNext()){  
+   System.out.println(itr.next());  
+
    }
+}
 }
