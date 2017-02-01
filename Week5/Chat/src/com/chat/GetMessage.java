8e361721bc5e53180cec8a87cd03de97e2764b22
satyapendem <sunilsmartguy005@gmail.com>
2017-02-01 16:06:30 +0530

    Added week5 with ChatApp

---
+++ Week5/Chat/src/com/chat/GetMessage.java
@@ -0,0 +1,23 @@
+package com.chat;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/GetMessage")
+public class GetMessage extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StringBuilder output = new StringBuilder();
+		
+		for(Message message: Users.messages){
+			output.append(message + "<br>");
+		}
+		
+		response.getOutputStream().print(output.toString());
+	}
+}
---
+++ Week5/Chat/src/com/chat/Message.java
@@ -0,0 +1,16 @@
+package com.chat;
+
+public class Message {
+	public String message;
+	public User user;
+	
+	public Message(User user, String message){
+		this.user = user;
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return (user.username != null ? user.username : user.ip) + ": " + message;
+	}
+}
