c72bbe521f28a954a569f84319e12e90e891ad52
satyapendem <sunilsmartguy005@gmail.com>
2017-05-10 17:43:08 +0530

    code mirror updates in week10

--- week10/Code Highlighter Complete/jsscript.js
+++ week10/Code Highlighter Complete/jsscript.js
@@ -18,124 +18,96 @@ editor1.setSize(540, 200);
    //$("#save").hide();
    //$("#edit").hide();
    // $('#buttons').hide();
-   $(document).on('mouseenter', '.code', function () {
-    $(this).find(".pre_icons").show();
-  });
-   $(document).on("mouseleave", ".code", function() {
-     $(this).find(".pre_icons").hide();
+   //MOUSEHOVER
+  $(document).on('mouseenter', '.code', function () {
+    $(this).find("#pre_icons").show();
    });
-   $("#show").click(function(){
-     $('#myModal').modal('hide');
-     //$("#save").show();
-     //$("#edit").show();
-     // $('#buttons').show();
-     var program=editor.getValue();
-     var code_div = document.getElementsByClassName("code");
-     var icon = $("#buttons");
-     if($(code_div).children().length<1){
-      $(code_div).append(program);
-      console.log("if");
+   $(document).on("mouseleave", ".code", function() {
+     $(this).find("#pre_icons").hide();
+   });
+  //Snippet div creation
+  var create_snippet= document.getElementById('show');
+  create_snippet.addEventListener('click',createsnippet);
+  function createsnippet(){
+    $('#myModal').modal('hide');
+    var program = editor.getValue();
+    var code_div=document.getElementsByClassName('.code');
+    var icons = document.getElementById('buttons');
+    if(code_div.childElementCount<1){
+      code_div.appendChild(program);
+      console.log("append");
     }
-    else{
-      code_div = document.createElement('div');
-      code_div.setAttribute('class','code');
-      // code_div.setAttribute('id',ids);
-      code_div.innerHTML = program;
-      $(".code").last().after(code_div);
-      // $("#"+ids).append(new_ele);
-      console.log("else");
-    }
-    $(code_div).each(function() {
-      debugger;
-      var $this = $(this),
-      $code = $this.html(),
-      $unescaped = $('<div/>').html($code).text();
-      $this.empty();
-      CodeMirror(this, {
-        value: $unescaped,
-        lineNumbers: true,
-        theme: "neo",
-        readOnly: "nocursor"
-      });   
-    });
-
-    new_ele = $('.pre_icons').clone();
-     //new_ele.removeAttr('id');
-     $(code_div).append(new_ele);
-     /*Click events for edit and download*/
-     
-     $('.edit').click(function(event){
-      $('#edit_modal').modal('show');
-       edit_div=$(this).parents('div.pre_icons').siblings('div.CodeMirror').find('div.CodeMirror-code');
-       edit_code = $(edit_div).clone();
-       content_code = $(edit_code).find('div.CodeMirror-linenumber').remove();
-       txt_for_update=$(edit_code).text();
-       console.log(txt_for_update);
-       editor1.setValue(txt_for_update);
+    else {
+        code_div = document.createElement('div');
+        code_div.setAttribute('class','code');
+        // code_div.setAttribute('id',ids);
+        code_div.innerHTML = program;
+        //debugger;
+         var add = document.getElementsByClassName('code');
+        $(".code").last().after(code_div);
+        // $("#"+ids).append(new_ele);
+        console.log("create new div");
+      }
+      $(code_div).each(function() {
+        var $this = $(this),
+        $code = $this.html(),
+        $unescaped = $('<div/>').html($code).text();
+        $this.empty();
+        CodeMirror(this, {
+          value: $unescaped,
+          lineNumbers: true,
+          theme: "neo",
+          readOnly: "nocursor"
+        });
       });
-      $('#update_code').click(function(){
-        $('#edit_modal').modal('hide');
-        console.log("updated clicked");
-        console.log(edit_div);
-        updated_text=editor1.getValue();
-        $(edit_div).text(updated_text);
-           //$(code_div).innerHTML(txt_for_update);
+      new_ele=document.getElementById('pre_icons');
+      var clone_ele=new_ele.cloneNode(true);
+      code_div.appendChild(clone_ele);
+
+      console.log(code_div);
+      editor.setValue("");
+      var name= document.getElementById('fileName');
+      name.value="";
+       /*Edit option listeners*/
+      var edit_code = document.querySelectorAll('.edit');
+      editor1.setValue("");
+      for(var i=0;i<edit_code.length;i++){
+        edit_code[i].addEventListener('click',function () {
+          $('#edit_modal').modal('show');
+          editor1.setValue("");
+          for_update = this.parentNode.parentNode;
+          console.log(for_update);
+          edit_div=for_update.getElementsByClassName('CodeMirror-code');
+          console.log(edit_div);
+          //linenumbers = edit_div[0].getElementsByClassName('CodeMirror-linenumber');
+          //console.log(linenumbers);
+          code_edit= edit_div[0].cloneNode(true);
+          linenumbers=code_div.getElementsByClassName('CodeMirror-linenumber');
+          for(i=0;i<linenumbers.length;i++){
+            linenumbers[i].remove();
+          }
+
+          //console.log(codeContent);
+          //for(i=0;i<edit_div.length;i++){
+            //if(edit_div[i].classList.contains('CodeMirror-linenumbers'))
+              //    console.log("found");
+
+          //  text=edit_div[i].textContent;
+          //}
+          //editor1.setValue(text);
+          document.getElementById('update_code').addEventListener('click',function(){
+            $('#edit_modal').modal('hide');
+              //console.log("inside update button");
+              //var updated_text=editor1.getValue();
+              //edit_div.innerHTML=updated_text;
+
+          });
+          //console.log(text);
+          //linenumbers = edit_div.getElementsByClassName('CodeMirror-linenumber');
+          //console.log(linenumbers);
+          //code_edit= edit_div.cloneNode(true);
+          //codeContent = code_edit.getElementsByClassName('CodeMirror-linenumber').remove();
+          //console.log(codeContent);
         });
-     $('.save').click(function(){
-      down_div=$(this).parents('div.pre_icons').siblings('div.CodeMirror').find('div.CodeMirror-code');
-      down_code = $(down_div).clone();
-      console.log(down_code);
-      content_code = $(down_code).find('div.CodeMirror-linenumber').remove();
-      txt_for_down=$(down_code).text();
-      console.log(txt_for_down);
-      var type=document.getElementById('language');
-      type=type.options[type.selectedIndex].value;
-      file_name=document.getElementById('fileName').value;
-      console.log(file_name);
-      if(file_name==""){
-        file_para="program";
       }
-      else{
-        file_para=file_name;
-      }
-      switch(type){
-        case "c": saveTextAsFile(file_para,"c");
-        break;
-        case "java": saveTextAsFile(file_para,"java");
-        break;
-        case "c++": saveTextAsFile(file_para,"cpp");
-        break;
-        case "php": saveTextAsFile(file_para,"php");
-        break;
-        case "markdown": saveTextAsFile(file_para,"md");
-        break      
-        case "plaintext": saveTextAsFile(file_para,"txt");
-        break      
-
-      }
-      function saveTextAsFile(file,type){
-        var textToWrite = txt_for_down;
-        var textFileAsBlob = new Blob([textToWrite],{
-          type:'text/plain'
-        });
-        var fileNameToSaveAs =file+"."+type;
-        var downloadLink = document.createElement("a");
-        downloadLink.download = fileNameToSaveAs;
-        downloadLink.innerHTML = "Download File";
-        if (window.URL != null){
-          downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-        }
-        else{
-          downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-          downloadLink.onclick = destroyClickedElement;
-          downloadLink.style.display = "none";
-          document.body.appendChild(downloadLink);
-        }
-        downloadLink.click();
-       }
-     });
-    });
-
-   
-
-
+    }
--- week10/Code Highlighter Complete/script.js
+++ week10/Code Highlighter Complete/script.js
@@ -24,7 +24,7 @@ editor1.setSize(560, 200);
    //$("#save").hide();
    //$("#edit").hide();
    // $('#buttons').hide();
-  
+
    $(document).on('mouseenter', '.code', function () {
     $(this).find(".pre_icons").show();
   });
@@ -36,13 +36,13 @@ editor1.setSize(560, 200);
      //$("#save").show();
      //$("#edit").show();
      // $('#buttons').show();
-
+      editor.setValue("");
      var program=editor.getValue();
      var code_div = $(".code");
      var icon = $("#buttons");
      if($(code_div).children().length<1){
       $(code_div).append(program);
-      console.log("if");
+      console.log("append");
     }
     else{
       code_div = document.createElement('div');
@@ -52,10 +52,10 @@ editor1.setSize(560, 200);
       //debugger;
       $(".code").last().after(code_div);
       // $("#"+ids).append(new_ele);
-      console.log("else");
+      console.log("create new div");
     }
     $(code_div).each(function() {
-      
+
       var $this = $(this),
       $code = $this.html(),
       $unescaped = $('<div/>').html($code).text();
@@ -65,15 +65,16 @@ editor1.setSize(560, 200);
         lineNumbers: true,
         theme: "neo",
         readOnly: "nocursor"
-      });   
+      });
     });
 
     new_ele = $('.pre_icons').clone();
      //new_ele.removeAttr('id');
      $(code_div).append(new_ele);
+
      /*Click events for edit and download*/
-   });  
-     
+   });
+
      $(document).on('click','.edit', function(event){
       $('#edit_modal').modal('show');
       editor1.setValue("");
@@ -81,11 +82,12 @@ editor1.setSize(560, 200);
        edit_div=$(for_update).find('div.CodeMirror-code');
        edit_code = $(edit_div).clone();
         document.getElementById("Up_fileName").value=file_name;
-       content_code = $(edit_code).find('div.CodeMirror-linenumber').remove();
+       //content_code = $(edit_code).find('div.CodeMirror-linenumber').remove();
        txt_for_update=$(edit_code).text();
        console.log(txt_for_update);
        editor1.setValue("");
        editor1.setValue(txt_for_update);
+
       });
       $('#update_code').click(function(){
         $('#edit_modal').modal('hide');
@@ -95,9 +97,9 @@ editor1.setSize(560, 200);
         $(for_update).find('.CodeMirror').remove();
         update_div = document.createElement('div');
         update_div.innerHTML = updated_text;
-        
+
          //for_update = $(edit_div).parents().find('.code');
-         $(for_update).prepend(update_div);        
+         $(for_update).prepend(update_div);
         //$(edit_div).html(updated_text);
            //$(code_div).innerHTML(txt_for_update);
            $(update_div).each(function() {
@@ -110,9 +112,9 @@ editor1.setSize(560, 200);
           lineNumbers: true,
           theme: "neo",
           readOnly: "nocursor"
-        });   
-       });
         });
+       });
+    });
      $(document).on('click','.save',function(){
       for_down=$(this).parents(".code");
       down_div=$(for_down).find('div.CodeMirror-code');
@@ -121,7 +123,7 @@ editor1.setSize(560, 200);
       content_code = $(down_code).find('div.CodeMirror-linenumber').remove();
       txt_for_down=$(down_code).text();
       console.log(txt_for_down);
-      var type=document.getElementById('language');
+      type=document.getElementById('language');
       type=type.options[type.selectedIndex].value;
       file_name=document.getElementById('fileName').value;
       console.log(file_name);
@@ -141,9 +143,9 @@ editor1.setSize(560, 200);
         case "php": saveTextAsFile(file_para,"php");
         break;
         case "markdown": saveTextAsFile(file_para,"md");
-        break      
+        break
         case "plaintext": saveTextAsFile(file_para,"txt");
-        break      
+        break
 
       }
       function saveTextAsFile(file,type){
@@ -167,8 +169,3 @@ editor1.setSize(560, 200);
         downloadLink.click();
        }
      });
-   
-
-   
-
-
