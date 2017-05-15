b29d3a48263b6dfdd4be1c54514751e10dbeaca2
satyapendem <sunilsmartguy005@gmail.com>
2017-05-15 17:04:05 +0530

    code formatter added

--- week10/Code Highlighter Complete/script1.js
+++ week10/Code Highlighter Complete/script1.js
@@ -1,3 +1,26 @@
+opts={
+    "indent_size": 4,
+    "indent_char": " ",
+    "indent_with_tabs": false,
+    "eol": "\n",
+    "end_with_newline": false,
+    "indent_level": 0,
+    "preserve_newlines": true,
+    "max_preserve_newlines": 10,
+    "space_in_paren": false,
+    "space_in_empty_paren": false,
+    "jslint_happy": true,
+    "space_after_anon_function": false,
+    "brace_style": "collapse",
+    "break_chained_methods": false,
+    "keep_array_indentation": true,
+    "unescape_strings": false,
+    "wrap_line_length": 0,
+    "e4x": false,
+    "comma_first": false,
+    "operator_position": "before-newline"
+}
+
 $('#language').on('change',function(){
     type=this.value;
 });
@@ -47,7 +70,6 @@ file_name=document.getElementById('fileName').value;
       console.log("create new div");
     }
     $(code_div).each(function() {
-
       var $this = $(this),
       $code = $this.html(),
       $unescaped = $('<div/>').html($code).text();
@@ -56,6 +78,7 @@ file_name=document.getElementById('fileName').value;
         value: $unescaped,
         lineNumbers: true,
         theme: "neo",
+        viewportMargin: Infinity,
         readOnly: "nocursor"
       });
     });
@@ -171,13 +194,13 @@ file_name=document.getElementById('fileName').value;
      /*end of save code*/
 /*beautify function to format code*/
 function formatCode(code) {
-  var formattedCode=js_beautify(code);
+  var formattedCode=js_beautify(code,opts);
   return formattedCode;
 }
 /*beautify html code*/
 function formatHtml(s) {
   console.log("in Html foramtter");
-  s=html_beautify(s);
+  s=html_beautify(s,opts);
 return s;
 }
 /*to check Whether html are not*/
